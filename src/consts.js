171c5a7ababab8435fc23c5317dfc5a83e54d084
Гендольф <ecehuh67@gmail.com>
2020-06-29 22:25:28 +0300

    changed colors of tetramino

--- src/consts.js
+++ src/consts.js
@@ -3,7 +3,7 @@ export const FIELD_SIZE = {
   height: 15,
 };
 
-export const COLORS = ['#FF00CC', '#2ab7ca', '#fed766 ', '#2243B6', '#e6e6ea'];
+export const COLORS = ['#ff1d58', '#0049B7', '#fff685', '#8458B3', '#f37736'];
 export const AMOUNT_OF_ROTATION = 4;
 
 export const KEYBOARD_KEYS = {
